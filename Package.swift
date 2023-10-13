fbc21b2f7ed36d24d4ca4b582595c7df3ac5bc98
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-13 11:04:21 +0300

    1.8.0-15

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-14"
+let bnbPackageVersion: Version = "1.8.0-15"
 
 let package = Package(
     name: "BNBSdkCore",
@@ -12,7 +12,7 @@ let package = Package(
         .library(
             name: "BNBSdkCore",
             targets: [
-                "BNBSdkCore_Target"
+                "BNBSdkCore"
             ]
         ),
     ],
@@ -21,7 +21,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "BNBSdkCore_Target",
+            name: "BNBSdkCore",
             url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-58-g46079e7d74/BNBSdkCore.zip",
             checksum: "b72695f653e5fde58b7e92ea3aeb862c4d4fa26cae07b53de528fd7a2bac2052"
         ),
